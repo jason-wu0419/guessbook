e5581ee97e8d7d59ebea1e02a2bdf3a0b58c5066
jason <10930325@dcsh.tp.edu.tw>
2020-12-17 12:06:27 +0800

    刪除留言

--- guestbook/urls.py
+++ guestbook/urls.py
@@ -21,4 +21,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('message/', include('web.urls')),
     path('',RedirectView.as_view(url='message')),
+    path("accounts/",include('django.contrib.auth.urls')),
 ]
--- web/views.py
+++ web/views.py
@@ -14,5 +14,9 @@ class MessageDetail(DetailView):
 class MessageCreate(CreateView):
     model = Message
     fields = '__all__'    
-    success_url = '/message/list/'
-    # reverse_lazy('msg_list')
+    success_url = reverse_lazy('msg_list')
+
+
+class MessageDelete(DeleteView):
+    model = Message
+    success_url = reverse_lazy('msg_list')  
